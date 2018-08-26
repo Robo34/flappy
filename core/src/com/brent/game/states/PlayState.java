70677046fe13f658fd8a85038e8e02e8b50a0940
Robo34 <pretorius@op.pl>
2018-08-26 08:41:38 +0200

    part8 yt flappy2 branch

--- core/src/com/brent/game/states/PlayState.java
+++ core/src/com/brent/game/states/PlayState.java
@@ -1,22 +1,31 @@
 package com.brent.game.states;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.brent.game.FlappyDemo;
 import com.brent.game.sprites.Bird;
+import com.brent.game.sprites.Tube;
 
 
 public class PlayState extends State {
     private Bird bird;
+    private Texture bg;
+    private Tube tube;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 300);
         cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
+        bg = new Texture("bg.png");
+        tube = new Tube(100);
     }
 
     @Override
     public void handleInput() {
-
+        if(Gdx.input.justTouched()){
+            bird.jump();
+        }
     }
 
     @Override
@@ -30,7 +39,10 @@ public class PlayState extends State {
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
+        sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
         sb.draw(bird.getTexture(),bird.getPosition().x,bird.getPosition().y);
+        sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+        sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
         sb.end();
     }
 
